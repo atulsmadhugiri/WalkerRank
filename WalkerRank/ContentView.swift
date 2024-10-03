e2145a4733b94f1de5b8ece268c048021ebcc93b
Atul Madhugiri <atulm@berkeley.edu>
2024-10-03 00:06:30 -0400

    Spruce up `ContentView` a little

--- WalkerRank/ContentView.swift
+++ WalkerRank/ContentView.swift
@@ -6,19 +6,20 @@ struct ContentView: View {
 
   var body: some View {
     VStack {
-      Text("Today's Step Count")
+      Text("Atul's Step Count")
         .font(.headline)
       Text("\(Int(healthStore.stepCount))")
         .font(.largeTitle)
         .fontWeight(.bold)
+        .contentTransition(.numericText())
 
-      Button("Refresh") {
+      Button("Refresh", systemImage: "arrow.clockwise.circle.fill") {
         healthStore.fetchSteps()
       }
-      .padding()
       .background(Color.blue)
       .foregroundColor(.white)
       .cornerRadius(10)
+      .buttonStyle(.bordered)
     }
     .task {
       let authorized = await healthStore.requestAuthorization()
