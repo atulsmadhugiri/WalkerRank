3dcb88dc39a015a6ed71c0175f58b90d0297b218
Atul Madhugiri <atulm@berkeley.edu>
2024-10-02 22:28:23 -0400

    Initial Commit

---
+++ WalkerRank/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WalkerRank
+//
+//  Created by atul on 10/2/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
